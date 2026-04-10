f3f39b65c8a629f74d1c1821ccdb99698bcbec75
Eduardo <eduardo.vchaves23@gmail.com>
2026-04-09 21:33:56 -0300

    Add website pinging functionality with logging
    Implement a keep-alive script that pings a website at regular intervals.

---
+++ main.py
@@ -0,0 +1,22 @@
+import logging
+import requests 
+import time
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+WEBSITE_URL = ''
+BREAK = 60 * 14 # 14 minutes, substitute fourteen for the number of minutes that you desire
+
+def ping():
+    try:
+        response = requests.get(WEBSITE_URL, timeout=10)
+        logging.info(f'Ping ok - status code: {response.status_code}')
+    except Exception as e:
+        logging.error(f'Ping error: {e}')
+
+
+if __name__ == "__main__":
+    logging.info('Initializing keep-alive')
+    while True:
+        ping()
+        time.sleep(BREAK)
